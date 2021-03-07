413101cb2440ca7e57d9c73b8f172c1d7f9a638f
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-03-07 16:55:57 -0500

    Fix login bug

--- src/customer-interface.js
+++ src/customer-interface.js
@@ -1,5 +1,8 @@
 import fetchData from './API-calls.js';
 import Hotel from './Hotel';
+import './images/user.png';
+import './css/customer-interface.scss';
+
 
 const checkLocalStorage = () => {
   if (localStorage.getItem('hotelOverlookLogin') === 'manager') {
@@ -17,9 +20,19 @@ const loadGuestLayout = (username) => {
   .then(allData => {
     let hotel = new Hotel(allData);
     let currentUser = hotel.guests.find(guest => guest.username === username);
-    console.log(hotel);
-    console.log(currentUser);
+    displayProfileInformation(currentUser)
   })
 }
 
+const displayProfileInformation = (currentUser) => {
+  document.querySelector('.profile-full-name').innerText = currentUser.name;
+  document.querySelector('.profile-username').innerText = currentUser.username;
+}
+
+const logOut = () => {
+  localStorage.removeItem('hotelOverlookLogin')
+  window.location.replace('./index.html')
+}
+
+document.querySelector('.log-out-button').addEventListener('click', logOut)
 window.addEventListener('load', checkLocalStorage);
