f0eb2f35208646e1cc81e702555e24b809271fc8
Cameron Mackintosh <cbmackintosh@outlook.com>
2021-03-07 11:17:55 -0500

    Introduce more comprehensive login pathways

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import './css/base.scss';
 
 const checkLocalStorage = () => {
-  
+  if (localStorage.getItem('hotelOverlookLogin') === 'manager') {
+    window.location.replace('./manager-interface.html');
+  } else if (localStorage.getItem('hotelOverlookLogin')) {
+    window.location.replace('./customer-interface.html');
+  } else {
+    null;
+  }
 }
 
 const login = () => {
   event.preventDefault()
   if (document.getElementById('password-field').value === 'overlook2021' && document.getElementById('username-field').value === 'manager')  {
-    localStorage.setItem('hotelOverlookLogin', 'manager')
-    window.location.replace('./manager-interface.html')
+    localStorage.setItem('hotelOverlookLogin', 'manager');
+    window.location.replace('./manager-interface.html');
   } else if (document.getElementById('password-field').value === 'overlook2021' && fetch('http://localhost:3001/api/v1/customers').then(response => response.json()).then(data => data.customers.map(customer => `customer${customer.id}`).includes(document.getElementById('username-field').value))) {
-    localStorage.setItem('hotelOverlookLogin', `${document.getElementById('username-field').value}`)
-    window.location.replace('./customer-interface.html')
+    localStorage.setItem('hotelOverlookLogin', `${document.getElementById('username-field').value}`);
+    window.location.replace('./customer-interface.html');
   }
 }
 
-document.querySelector('.login-button').addEventListener('click', login)
+document.querySelector('.login-button').addEventListener('click', login);
+window.addEventListener('load', checkLocalStorage);
--- src/manager-interface.js
+++ src/manager-interface.js
@@ -2,7 +2,17 @@
 import fetchData from './API-calls.js';
 import Hotel from './Hotel';
 
-const loadApp = () => {
+const checkLocalStorage = () => {
+  if (localStorage.getItem('hotelOverlookLogin') === 'manager') {
+    loadManagerInterface();
+  } else if (localStorage.getItem('hotelOverlookLogin')) {
+    window.location.replace('./customer-interface.html');
+  } else {
+    window.location.replace('./index.html');
+  }
+}
+
+const loadManagerInterface = () => {
   fetchData()
   .then(allData => {
     let hotel = new Hotel(allData);
@@ -10,4 +20,4 @@ const loadApp = () => {
   })
 }
 
-window.addEventListener('load', loadApp)
+window.addEventListener('load', checkLocalStorage)
